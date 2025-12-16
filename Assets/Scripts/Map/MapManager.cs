4ead025cd9aaaef2bb1b542c7b9e06e08a6342b2
Mana <dmcooper2412@gmail.com>
2025-12-15 20:34:57 -0800

    Day 13 - Filled out Passage Scene

--- Assets/Scripts/Map/MapManager.cs
+++ Assets/Scripts/Map/MapManager.cs
@@ -12,6 +12,8 @@ namespace Map
         private readonly int _bloodMoorSceneIndex = 3;
         private readonly int _denOfEvilSceneIndex = 4;
         private readonly int _coldPlainsSceneIndex = 5;
+        private readonly int _passageToBloodRavenSceneIndex = 6;
+        private readonly int _burialGroundsSceneIndex = 7;
         
         private void OnTriggerEnter2D(Collider2D other)
         {
@@ -19,26 +21,33 @@ namespace Map
             {
                 return;
             }
-            
-            if (gameObject.name == "Tilemap_LoadTown")
+
+            switch (gameObject.name)
             {
-                Debug.Log("Load Town");
-                SceneManager.LoadSceneAsync(_townSceneIndex);
-            }
-            else if (gameObject.name == "Tilemap_LoadBloodMoor")
-            {
-                Debug.Log("Load Blood Moor");
-                SceneManager.LoadSceneAsync(_bloodMoorSceneIndex);
-            }
-            else if (gameObject.name == "Tilemap_LoadDenOfEvil")
-            {
-                Debug.Log("Load Den Of Evil");
-                SceneManager.LoadSceneAsync(_denOfEvilSceneIndex);
-            } 
-            else if (gameObject.name == "Tilemap_LoadColdPlains")
-            {
-                Debug.Log("Load Cold Plains");
-                SceneManager.LoadSceneAsync(_coldPlainsSceneIndex);
+                case "Tilemap_LoadTown":
+                    Debug.Log("Load Town");
+                    SceneManager.LoadSceneAsync(_townSceneIndex);
+                    break;
+                case "Tilemap_LoadBloodMoor":
+                    Debug.Log("Load Blood Moor");
+                    SceneManager.LoadSceneAsync(_bloodMoorSceneIndex);
+                    break;
+                case "Tilemap_LoadDenOfEvil":
+                    Debug.Log("Load Den Of Evil");
+                    SceneManager.LoadSceneAsync(_denOfEvilSceneIndex);
+                    break;
+                case "Tilemap_LoadColdPlains":
+                    Debug.Log("Load Cold Plains");
+                    SceneManager.LoadSceneAsync(_coldPlainsSceneIndex);
+                    break;
+                case "Tilemap_LoadPassageToBloodRaven":
+                    Debug.Log("Load Passage To Blood Raven");
+                    SceneManager.LoadSceneAsync(_passageToBloodRavenSceneIndex);
+                    break;
+                case "Tilemap_LoadBurialGrounds":
+                    Debug.Log("Load Burial Grounds");
+                    SceneManager.LoadSceneAsync(_burialGroundsSceneIndex);
+                    break;
             }
         }
     }
